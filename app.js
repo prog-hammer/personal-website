aa9d1888c35a893ba7b31814bea3e7cab0e7f8c0
Lakshya Bansal <bansallakshya94128@gmail.com>
2019-10-29 00:20:57 +0530

    Data and Time Fields Added

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     db.collection('queries').add({
         name: form.name.value,
         email: form.email.value,
         subject: form.subject.value,
-        message: form.message.value
+        message: form.message.value,
+        date: date,
+        time: time
     });
     form.name.value="";
     form.email.value="";
